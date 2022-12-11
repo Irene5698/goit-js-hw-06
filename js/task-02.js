70ce1992c35338329ace574b47dfd81b678a70c8
Irene5698 <110497689+Irene5698@users.noreply.github.com>
2022-12-11 20:44:29 +0200

    done

--- js/task-02.js
+++ js/task-02.js
@@ -7,33 +7,18 @@ const ingredients = [
   'Condiments',
 ];
 
-
-
 const list = document.getElementById("ingredients");
-const oneItem = document.createElement("li");
-oneItem.textContent = 'Potatoes';
-oneItem.classList = ("item")
 
-const twoItem = document.createElement("li");
-twoItem.textContent = 'Mushrooms';
-twoItem.classList = ("item")
+for (let i = 0; i < ingredients.length; i++){
+  let list = document.createElement('li');
+  list.innerText=ingredients[i];
+   document.querySelector("#ingredients").appendChild(list);
+   list.classList = ("item")
+}
 
-const threeItem = document.createElement("li");
-threeItem.textContent = 'Garlic';
-threeItem.classList = ("item")
-
-const forItem = document.createElement("li");
-forItem.textContent = 'Tomatos';
-forItem.classList = ("item")
-
-const fiveItem = document.createElement("li");
-fiveItem.textContent = 'Herbs';
-fiveItem.classList = ("item")
-
-const sixItem = document.createElement("li");
-sixItem.textContent = 'Condiments';
-sixItem.classList = ("item")
-
-
-list.append(oneItem,twoItem,threeItem, forItem, fiveItem, sixItem )
 console.log(list)
+
+
+
+
+
